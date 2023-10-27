2a57400971ea0edff6e4ac4b8ad67ae64a906e02
zhihe.rong <zhihe.rong@qiyuesuo.com>
2023-10-27 18:42:41 +0800

    chore:vue3+webpack搭建

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const name = ref<string>('th')
+</script>
+
+<template>
+    <div>
+        halo,webpack {{ name }}
+        <div>1122</div>
+    </div>
+</template>
+
+<style>
+</style>
